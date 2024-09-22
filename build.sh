c96b15f5d7ea3b792870818aacacf38c75310056
stamrakar <stamrakar@Ubuntu.myguest.virtualbox.org>
2024-09-22 11:49:34 -0600

    Defconfig file good

--- build.sh
+++ build.sh
@@ -15,12 +15,15 @@ cd `dirname $0`
 if [ ! -e buildroot/.config ]
 then
 	echo "MISSING BUILDROOT CONFIGURATION FILE"
+	echo "I am here"
 
 	if [ -e ${AESD_MODIFIED_DEFCONFIG} ]
 	then
 		echo "USING ${AESD_MODIFIED_DEFCONFIG}"
+		echo "I am here 2"
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_MODIFIED_DEFCONFIG_REL_BUILDROOT}
 	else
+		echo "I am here 3"
 		echo "Run ./save_config.sh to save this as the default configuration in ${AESD_MODIFIED_DEFCONFIG}"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
 		make -C buildroot defconfig BR2_EXTERNAL=${EXTERNAL_REL_BUILDROOT} BR2_DEFCONFIG=${AESD_DEFAULT_DEFCONFIG}
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Script to distclean buildroot directory
+DIRBR=/home/stamrakar/assignment-4-sota6640/buildroot
+cd ${DIRBR}
+make distclean
