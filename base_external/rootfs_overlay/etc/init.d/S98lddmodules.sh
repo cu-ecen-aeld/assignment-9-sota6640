bbba6c1ec1d08ec6bd9f282089904680751e407c
stamrakar <stamrakar@Ubuntu.myguest.virtualbox.org>
2024-10-17 23:57:35 -0600

    faulty driver mismatch fix

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -3,14 +3,14 @@
 case "$1" in
     start)
         echo "Startup to load the scull and the faulty driver"
-        /usr/bin/module_load
-        /usr/bin/scull_load faulty
+        /usr/bin/module_load faulty
+        /usr/bin/scull_load 
         modprobe hello
         ;;
     stop)
         echo "Shutdown to unload the scull and the faulty driver"
-        /usr/bin/module_unload
-        /usr/bin/scull_unload faulty
+        /usr/bin/module_unload faulty 
+        /usr/bin/scull_unload 
         rmmod hello
         ;;
     *)
