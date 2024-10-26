44360b6361df27aba5588e89679fbd0a2502414c
stamrakar <stamrakar@Ubuntu.myguest.virtualbox.org>
2024-10-26 02:18:51 -0600

    makefile, init scripts, Config.in added

---
+++ base_external/package/aesdchar/aesdchar.mk
@@ -0,0 +1,29 @@
+##############################################################
+#
+# AESDCHAR
+#
+##############################################################
+
+
+#TODO: Fill up the contents below in order to reference your assignment 3 git contents
+AESDCHAR_VERSION = '6f1c2deb6c06bc1379ea216f06d84def00b38929'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+AESDCHAR_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-sota6640.git'
+AESDCHAR_SITE_METHOD = git
+AESDCHAR_GIT_SUBMODULES = YES
+
+AESDCHAR_MODULE_SUBDIRS = aesd-char-driver
+
+
+define AESDCHAR_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
+endef
+
+
+
+#using kernel-module is just not sufficient, need generic-package as well
+$(eval $(kernel-module))
+$(eval $(generic-package))
--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -6,12 +6,14 @@ case "$1" in
         /usr/bin/module_load faulty
         /usr/bin/scull_load 
         modprobe hello
+        /usr/bin/aesdchar_load
         ;;
     stop)
         echo "Shutdown to unload the scull and the faulty driver"
         /usr/bin/module_unload faulty 
         /usr/bin/scull_unload 
         rmmod hello
+        /usr/bin/aesdchar_unload
         ;;
     *)
         echo "Usage: $0 {start|stop}"
